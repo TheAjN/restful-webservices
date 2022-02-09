f23e61d3af8793c642af3e5c772f50e4f734039b
TheAjN <abhisheknaik6@gmail.com>
2022-02-09 23:06:20 +0530

    Activating debug logs @ learning about @Path Variable

--- src/main/java/com/ajn/rest/webservices/restfulwebservices/Controller/HelloWorldController.java
+++ src/main/java/com/ajn/rest/webservices/restfulwebservices/Controller/HelloWorldController.java
@@ -2,14 +2,14 @@ package com.ajn.rest.webservices.restfulwebservices.Controller;
 
 import com.ajn.rest.webservices.restfulwebservices.Bean.helloWorldBean;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
-
 
 @RestController //A controller that can handle REst requests
 public class HelloWorldController {
 
     //@RequestMapping(path = "/hello-world", method = RequestMethod.GET)
-    @GetMapping(path = "/hello-world")
+    @GetMapping(path = "/helloworld")
     public String helloWorld(){
         return "Hello World!";
     }
@@ -22,4 +22,12 @@ public class HelloWorldController {
     public helloWorldBean helloWorldBean(){
         return new helloWorldBean("Hello World");
     }
+
+    //the {name} inside the curly brackets should match the variable as provided in the parameter
+    //value of {name} must be provided in the url, since there is no response created for "/hello-world-bean/path-variable"
+    @GetMapping(path="/hello-world-bean/path-variable/{name}")
+    public helloWorldBean helloWorldPathVariable(@PathVariable String name){
+        return new helloWorldBean(String.format("Hello World, %s",name));
+    }
+
 }
